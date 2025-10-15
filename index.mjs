5c28accf32295a0e4208817ea19ef795bbfbd869
pairavat <61785061+pairavat@users.noreply.github.com>
2025-10-15 14:44:04 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -88,7 +88,7 @@ app.get("/", (req, res) => {
 });
 
 // console.log(BigInt('123'));
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(process.env.DATABASE_URL);
